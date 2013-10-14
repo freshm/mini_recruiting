61b3a6df80d9488b6756d595a7671e3f2f8cc89a
Marvin Ahlgrimm <marv.ahlgrimm@gmail.com>
2013-10-14 14:48:22 +0200

    Created Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiniRecruiting::Application.config.secret_token = '178fd64612488777b446f9fd690ec03e4d6b310a6e115f08ed598c9b2b582941e9cf3b91ee70fd8a8f3db1ed6956fdaa0f050c843fe8c1a7d828ed0b63009ab4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+MiniRecruiting::Application.config.session_store :cookie_store, key: '_mini-recruiting_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# MiniRecruiting::Application.config.session_store :active_record_store
