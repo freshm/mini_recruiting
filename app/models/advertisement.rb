317b7df562ea60f58325bd7b84b77d7e35108f42
Marvin Ahlgrimm <marv.ahlgrimm@gmail.com>
2013-10-15 17:02:45 +0200

    Added logic to the advertisement show page

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -5,4 +5,19 @@ class Advertisement < ActiveRecord::Base
   attr_accessible :description, :location, :requirement, :title
 
   validates_presence_of :description, :location, :requirement, :title, :admin_id
+  
+  def already_taken_by_user?(user)
+    if user.job_applications.where(self.id).exists?
+      true
+    else
+      false
+    end
+  end
+  
+  def display_user_details(user)
+    application = user.job_applications.find_by_advertisement_id(self.id)
+    wage = application.wage || "You haven't entered a wage"
+    application.note = "You haven't entered a note" if application.note.empty?
+    [wage, application.note]
+  end
 end
